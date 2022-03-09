862d1df43e490567a889396a8dd71280d9b5e64b
Luk6785 <buiman127@gmail.com>
2022-03-09 17:47:30 +0700

    Deploy blog

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,11 +1,23 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'A simple programe convert string to hex by assembly',
+    description: `Một chương trình đơn giản viết bằng assembly chuyển chuỗi ngắn sang hex`,
+    imgSrc: '/static/images/simple_assembly.png',
+    href: 'https://github.com/Luk6785/simple_string_to_hex_by_assembly',
+  },
+  {
+    title: 'A simple client server by Winform',
+    description: `Một chương trình về giao thức TCP/IP đơn giản với chức năng: tham gia room, trò chuyện bí mật
+    , xem thông tin thành viên, đổi tên, chat qua lại giữa các room,....`,
+    imgSrc: '/static/images/client_servers.png',
+    href: 'https://github.com/Luk6785/simple_client_server',
+  },
+  {
+    title: 'A simple scan signature',
+    description: `Một chương trình quét hex signature đơn giản chuyển signature sang binary rồi so với từng 
+    path file trong thư mục tìm được`,
+    imgSrc: '/static/images/simple_scan.png',
+    href: 'https://github.com/Luk6785/simple_scan_hex_signature',
   },
   {
     title: 'The Time Machine',
